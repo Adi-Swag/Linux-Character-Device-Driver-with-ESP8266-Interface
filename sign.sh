544ee36a645232ccf7b733153e7ce9663598604d
Sai Adithya <f20220584@hyderabad.bits-pilani.ac.in>
2026-04-18 00:35:50 +0530

    Signing added to make file itself

--- sign.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-KDIR=/lib/modules/$(uname -r)/build
-SIGN_FILE=$KDIR/scripts/sign-file
-
-KEY=~/module-signing/MOK.key
-CERT=~/module-signing/MOK.crt
-
-$SIGN_FILE sha256 $KEY $CERT $1
